016321e2ae9c3b22d218dccb3f572447a609030c
CristianP1 <perezcristian730@gmail.com>
2023-06-19 19:39:22 -0500

    agregando el menu mobile

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const menu_login = document.querySelector(".navbar-email")
 const descktop_menu = document.querySelector(".desktop-menu")
+const burmenu = document.querySelector(".menu")
+const menuMobile = document.querySelector(".mobile-menu")
 
 menu_login.addEventListener("click", toggleMenu)
+burmenu.addEventListener("click", toggleMobileMenu)
 
 function toggleMenu(){
     descktop_menu.classList.toggle("inactive")
+}
+
+function toggleMobileMenu() {
+    menuMobile.classList.toggle("inactive")
 }
